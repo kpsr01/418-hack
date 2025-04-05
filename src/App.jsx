f939bfc4594a7a9ca1354dd8598f3ccb2d2a6141
kpsr01 <pranavsuhasreddykonduru@gmail.com>
2025-04-06 02:40:28 +0530

    modal added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 // src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
 import Scanner from "./components/Scanner";
 import SnackResult from "./components/SnackResult";
+import Rewards from './components/Rewards';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Landing />
-      <SnackResult />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Landing />
+              <SnackResult />
+            </>
+          } />
+          <Route path="/home" element={<>
+              <Landing />
+              <SnackResult />
+            </>} />
+          <Route path="/rewards" element={<Rewards />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
